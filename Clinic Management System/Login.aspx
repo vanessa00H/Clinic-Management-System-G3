25f69f8cfea444968af7ecf67ebac012c7d8844c
Vanessa <hsma2337@gmail.com>
2026-04-07 23:05:16 +0300

    h
    g

---
+++ Clinic Management System/Login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Clinic_Management_System.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtUsername" runat="server" placeholder="Username"></asp:TextBox>
+<br />
+
+<asp:TextBox ID="txtPassword" runat="server" TextMode="Password" placeholder="Password"></asp:TextBox>
+<br />
+
+<asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+<br />
+
+<asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Clinic Management System/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clinic_Management_System
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+             string connStr = ConfigurationManager.ConnectionStrings["ClinicDBConnection"].ConnectionString;
+
+             using (SqlConnection conn = new SqlConnection(connStr))
+             {
+                string query = "SELECT * FROM Users WHERE Username=@Username AND Password=@Password";
+
+              SqlCommand cmd = new SqlCommand(query, conn);
+              cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+              cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+              conn.Open();
+              SqlDataReader reader = cmd.ExecuteReader();
+
+              if (reader.Read())
+              {
+                Response.Redirect("Default.aspx");
+              }
+              else
+              {
+                lblMessage.Text = "Invalid username or password";
+              }
+        }
+    }
+}
+}
